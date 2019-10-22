7cb259ed25f6f1e8473647d72c171ebf2baa529e
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-22 12:24:17 -0700

    update path to import module code

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.use('*', express.static('public'));
+app.use('/:listingid', express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/:listingid', (req, res) => {
   res.end();
 });
 
