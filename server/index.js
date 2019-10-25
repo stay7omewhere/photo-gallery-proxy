1e24918d360b82f775fc86b26be205b4526678c4
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-25 16:10:59 -0700

    install request, make get request to module's IP

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const request = require('request');
 
 app.use('/:listingid', express.static('public'));
 
-app.get('/:listingid', (req, res) => {
-  res.end();
+app.get('/photos/:listingid', (req, res) => {
+  request(`http://18.217.122.245/photos/${req.params.listingid}`, (error, response, body) => {
+    if (error) return res.end();
+    res.send(JSON.parse(body));
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
