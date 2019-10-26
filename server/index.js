05066013d104f049467bfbbc94cb61c36f6991f7
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-25 19:39:34 -0700

    adding additional modules, get handlers

--- server/index.js
+++ server/index.js
@@ -12,4 +12,18 @@ app.get('/photos/:listingid', (req, res) => {
   });
 });
 
+app.get('/reviews', (req, res) => {
+  request('http://18.219.230.255/reviews', (error, response, body) => {
+    if (error) return res.end();
+    res.send(JSON.parse(body));
+  });
+});
+
+// app.get('/api/recommendations', (req, res) => {
+//   request('http://ec2-13-58-95-198.us-east-2.compute.amazonaws.com:3004/api/recommendations', (error, response, body) => {
+//     if (error) return res.end();
+//     res.send(JSON.parse(body));
+//   });
+// });
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
