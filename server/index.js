90a392e60b5327d60bed42cea99aac111723a552
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-25 19:48:17 -0700

    commented out get handlers

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const request = require('request');
 
 app.use('/:listingid', express.static('public'));
 
+// for photos module
 app.get('/photos/:listingid', (req, res) => {
   request(`http://18.217.122.245/photos/${req.params.listingid}`, (error, response, body) => {
     if (error) return res.end();
@@ -12,6 +13,50 @@ app.get('/photos/:listingid', (req, res) => {
   });
 });
 
+// for checkout module
+// app.get('/listing', (req, res) => {
+//   request('http://3.17.59.236/listing', (error, response, body) => {
+//     if (error) return res.end();
+//     res.send(JSON.parse(body));
+//   });
+// });
+
+// app.get('/currentCalendar', (req, res) => {
+//   request('http://3.17.59.236/currentCalendar', (error, response, body) => {
+//     if (error) return res.end();
+//     res.send(JSON.parse(body));
+//   });
+// });
+
+// app.get('/monthAndYear', (req, res) => {
+//   request('http://3.17.59.236/monthAndYear', (error, response, body) => {
+//     if (error) return res.end();
+//     res.send(JSON.parse(body));
+//   });
+// });
+
+// app.get('/month', (req, res) => {
+//   request('http://3.17.59.236/month', (error, response, body) => {
+//     if (error) return res.end();
+//     res.send(JSON.parse(body));
+//   });
+// });
+
+// app.get('/nextCalendar', (req, res) => {
+//   request('http://3.17.59.236/nextCalendar', (error, response, body) => {
+//     if (error) return res.end();
+//     res.send(JSON.parse(body));
+//   });
+// });
+
+// app.get('/previousCalendar', (req, res) => {
+//   request('http://3.17.59.236/previousCalendar', (error, response, body) => {
+//     if (error) return res.end();
+//     res.send(JSON.parse(body));
+//   });
+// });
+
+// for reviews module
 app.get('/reviews', (req, res) => {
   request('http://18.219.230.255/reviews', (error, response, body) => {
     if (error) return res.end();
@@ -19,8 +64,17 @@ app.get('/reviews', (req, res) => {
   });
 });
 
+// for recommendations module
 // app.get('/api/recommendations', (req, res) => {
 //   request('http://ec2-13-58-95-198.us-east-2.compute.amazonaws.com:3004/api/recommendations', (error, response, body) => {
+//     if (error) return res.end();
+//     res.send(JSON.parse(body));
+//   });
+// });
+
+// for experiences module
+// app.get('/experiences', (req, res) => {
+//   request('http://TOADD/experiences', (error, response, body) => {
 //     if (error) return res.end();
 //     res.send(JSON.parse(body));
 //   });
