a017c3ffe6bf660027bebce2fd33f91632e5a6d9
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-26 10:09:27 -0700

    remove router

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const router = require('./router.js');
 
 app.use('/:listingid', express.static('public'));
-app.use('/', router);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
